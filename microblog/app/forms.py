cccbcfd0c9c217cadfa6321fac640920f83df52d
atlrwt <atlrwt@gmail.com>
2023-04-12 16:23:42 +0800

    Facelift

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -8,7 +8,7 @@ from wtforms.validators import Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
