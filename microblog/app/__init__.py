b4f4acdee27c75fd05da82da2c2404779c1498b8
atlrwt <atlrwt@gmail.com>
2023-04-14 22:18:24 +0800

    l18n and L10n

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -10,6 +10,10 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
+from flask_babel import Babel, lazy_gettext as _l
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,9 +21,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -48,5 +54,10 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.info('Microblog startup')
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_locale)
 
 from app import routes, models, errors
