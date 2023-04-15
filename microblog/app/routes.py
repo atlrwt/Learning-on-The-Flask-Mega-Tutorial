dcd64cc2bb2271534a0425aef014bf1a28e6d6bc
atlrwt <atlrwt@gmail.com>
2023-04-15 14:11:19 +0800

    Ajax

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -20,6 +20,9 @@ from app.email import send_password_reset_email
 from app.forms import ResetPasswordForm
 from flask import g
 from flask_babel import _, get_locale
+from langdetect import detect, LangDetectException
+from flask import jsonify
+from app.translate import translate
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -27,7 +30,11 @@ from flask_babel import _, get_locale
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -187,3 +194,10 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+        request.form['source_language'],
+        request.form['dest_language'])})
